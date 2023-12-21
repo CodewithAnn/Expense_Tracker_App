9ac47d12af8cc2508d0eeace6ac5320016fff3fb
Anand jha <codewithann@gmail.com>
2023-12-21 20:56:47 +0530

    added models

---
+++ lib/models/expense.dart
@@ -0,0 +1,12 @@
+class Expense {
+  Expense(
+      {required this.title,
+      required this.iD,
+      required this.amount,
+      required this.date});
+
+  final String title;
+  final String iD;
+  final double amount;
+  final DateTime date;
+}
