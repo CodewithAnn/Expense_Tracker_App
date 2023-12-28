0d4444a5079493b1e3e3856745237ec153ceaa20
Anand jha <codewithann@gmail.com>
2023-12-28 22:49:48 +0530

    added theme in Expensy

---
+++ lib/Components/Theme/ktheme.dart
@@ -0,0 +1,29 @@
+import 'package:expense_tracker/Components/const/kcolors.dart';
+import 'package:flutter/material.dart';
+
+var kThemeData = ThemeData().copyWith(
+  colorScheme: kColorScheme,
+  appBarTheme: const AppBarTheme().copyWith(
+    backgroundColor: kColorScheme.onPrimaryContainer,
+    foregroundColor: kColorScheme.primaryContainer,
+  ),
+  cardTheme: const CardTheme().copyWith(
+    color: kColorScheme.secondaryContainer,
+    margin: const EdgeInsets.symmetric(
+      vertical: 9,
+      horizontal: 15,
+    ),
+  ),
+  elevatedButtonTheme: ElevatedButtonThemeData(
+    style: ElevatedButton.styleFrom(
+      backgroundColor: kColorScheme.primaryContainer,
+    ),
+  ),
+  textTheme: ThemeData().textTheme.copyWith(
+        titleLarge: TextStyle(
+          fontWeight: FontWeight.bold,
+          color: kColorScheme.onSecondaryContainer,
+          fontSize: 24,
+        ),
+      ),
+);
---
+++ lib/Components/const/kcolors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+var kColorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromRGBO(57, 99, 192, 1),
+);
